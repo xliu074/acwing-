243eb69a20333e7c4709692253c5a892e98d0827
xliu074 <gmarkanderson025@gmail.com>
2024-03-05 18:43:33 +0800

    dfs基础

--- acwing/Acwing 846. 树的重心.cpp
+++ acwing/Acwing 846. 树的重心.cpp
@@ -1,31 +1,25 @@
 #include<iostream>
-#include<cstring>
 #include<algorithm>
-#include<queue>
+#include<cstring>
 
 using namespace std;
 
 const int N = 100010, M = N * 2;
-int h[N], e[M], ne[M], idx;
+int h[N], e[M], ne[M], idx, n;
+int ans = N;
 bool st[N];
-int ans = N;//
-int n;
-
 void add(int a, int b)
 {
-    e[idx] = b; 
-    ne[idx] = h[a];
-    h[a] = idx ++ ;
+    e[idx] = b; ne[idx] = h[a]; h[a] = idx ++;
 }
 int dfs(int u)
 {
     st[u] = true;
-    int size, sum;
-    for(int i = h[u]; i != -1; i = ne[i] )
+    int sum = 0, size = 0;
+    for(int i = h[u]; i != -1; i = ne[i])
     {
         int j = e[i];
         if(st[j]) continue;
-
         int s = dfs(j);
         size = max(size, s);
         sum += s;
@@ -36,12 +30,13 @@ int dfs(int u)
 }
 int main()
 {
-    cin >> n;
-    for(int i = 0; i < n; i ++ )
+    scanf("%d", &n);
+    memset(h, -1, sizeof h);
+    for(int i = 0; i < n - 1; i ++ )
     {
         int a, b;
         scanf("%d%d", &a, &b);
-        add(a, b), add(b, a);
+        add(a, b); add(b, a);
     }
     dfs(1);
     cout << ans << endl;
